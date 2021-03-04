311f58eb33ed7b46cbb16d2d1bfd7fedfa235bc8
Jesus R <qlm611@my.utsa.edu>
2021-03-04 01:19:10 -0600

    adding bootstrap and update header, footer, main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { Container } from 'react-bootstrap'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      My App
+      < Header />
+      
+        <main className="py-3">
+          < Container >
+            <h1>
+              Welcome to Silkie Trading Technologies!
+            </h1>
+          </Container>
+        </main>
+
+      < Footer />
     </div>
   );
 }
